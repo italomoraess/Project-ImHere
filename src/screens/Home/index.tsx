7774a582ca7d05441898a66bdc7dbc2a5f99cfc8
italomoraess <italomoraes.sth@gmail.com>
2024-03-05 17:53:50 -0300

    add: button and create form add participant

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,19 +1,28 @@
-import { Text, TextInput, View } from "react-native";
+import { Button, Text, TextInput, TouchableOpacity, View } from "react-native";
 import { styles } from "./styles";
 
 export default function Home() {
   const date = new Date();
+
+  function handleParticipantAdd() {
+    console.log("Clicou aqui!!!")
+  }
+
   return(
     <View style={styles.container}>
       <Text key={1} style={styles.eventName} >Nome do evento</Text>
       <Text key={2} style={styles.eventDate}>{date.toUTCString()}</Text>
-
-      <TextInput 
-        style={styles.input}
-        placeholder="Nome do participante"
-        placeholderTextColor='#6b6b6b'
-        keyboardType="numeric"
-      />
+      <View style={styles.form}>
+        <TextInput 
+          style={styles.input}
+          placeholder="Nome do participante"
+          placeholderTextColor='#6b6b6b'
+          keyboardType="numeric"
+        />
+        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+      </View>  
     </View>
   );
 }
